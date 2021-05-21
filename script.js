77cf12c80a59e00da2445776b8f4e594e3854180
ThangVPham <thangpham1412@gmail.com>
2021-05-21 18:50:03 -0400

    change

--- script.js
+++ script.js
@@ -2,4 +2,6 @@ console.log("Hello World");
 for(let i =0; i<=5; i++){
     console.log(`Value of i is: ${i}`);
 }
-console.log("Hi");
+console.log("Hello");
+console.log("World");
+console.log("Test Test Test");
